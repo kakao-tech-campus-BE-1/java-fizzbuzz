5d01f8c0001344ba378888459c20ee15df19d189
Kim Dayeong <gloria030114@gmail.com>
2024-06-04 20:06:34 +0900

    feat: add for loop in main logic
    - add for loop 1 to 100

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        for (int i = 1; i < 101; i++) {
+            // TODO : implement the logic
+        }
     }
 }
