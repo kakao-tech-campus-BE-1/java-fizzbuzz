dfd9156b478fbb3d166eb06aa9849d6f5fc4a2d8
young-in <hardworkingini@gmail.com>
2024-06-04 20:10:30 +0900

    [feat] issue 3 completed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,14 @@
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+    }
+
+    static String fizzbuzz(int n) {
+        if (n % 3 == 0 && n % 5 == 0) {
+            return "FizzBuzz";
+        }
+        else return null;
     }
 }
