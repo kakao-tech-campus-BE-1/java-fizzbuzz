acbf670775e0a5cf37df1c9d9e91aef99154eea3
Youngin Kim <hardworkingini@gmail.com>
2024-06-04 20:15:16 +0900

    [feat] issue 3 completed (#5)
    Co-authored-by: Kim Dayeong <gloria030114@gmail.com>

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,16 @@
 public class Main {
 
     public static void main(String[] args) {
+      
         for (int i = 1; i < 101; i++) {
             // TODO : implement the logic
         }
+    }
+
+    static String fizzbuzz(int n) {
+        if (n % 3 == 0 && n % 5 == 0) {
+            return "FizzBuzz";
+        }
+        else return null;
     }
 }
